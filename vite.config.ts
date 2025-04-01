70076d1acc7e989affdbb7699f06b04ba84097f4
jrdelgado <interamaster@gmail.com>
2025-04-01 19:17:39 +0200

    tryng fix before cat on main screen

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -17,6 +16,11 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    commonjsOptions: {
+      include: [/lucide-react/, /node_modules/],
     },
   },
 }));
